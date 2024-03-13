ebbfd91e21bc7cf8eab772d106601e3d38caa16b
Saurabh <136837795+Saurabh9527@users.noreply.github.com>
2024-03-13 13:35:52 +0530

    completed

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -6,7 +6,7 @@ dotenv.config()
 
 //~ database Connection
 const databaseConnection = () =>{
-    // console.log(process.env.MONGO_URL);
+    
     mongoose.connect(process.env.MONGO_URL).
     then(()=>{
         console.log("Database Connected Successfully");
