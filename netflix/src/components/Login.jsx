e7fa80b42b0a4e2f412b045c05f1b9c864d4c74d
Saurabh Niwate <niwatesaurabh2000@gmail.com>
2024-03-24 10:56:23 +0530

    README file updated

--- netflix/src/components/Login.jsx
+++ netflix/src/components/Login.jsx
@@ -90,7 +90,7 @@ const Login = () => {
       </div>
       <form onSubmit={handleSubmit} className='w-11/12 p-2 absolute my-20 md:my-14 bg-black mx-auto left-0 right-0 md:w-4/12 md:p-12 text-white bg-opacity-80 outline-none rounded-lg'>
         {isLogin ? <h1 className='  font-bold text-3xl mb-3 '>Sign In</h1> : <h1 className='font-bold text-3xl mb-3'>Sign Up</h1>}
-        {!isLogin && <input className='mx-5 p-3 w-10/12 md:w-11/12 my-4 md:mx-0 rounded-lg bg-gray-800' placeholder='Enter Name' type="text" value={fullName} onChange={(e) => setFullName(e.target.value)} />}
+        {!isLogin && <input className='mx-5 p-3 w-10/12 md:w-11/12 my-4 md:mx-0 rounded-lg bg-gray-800' placeholder='Enter Name' type="text"  value={fullName} onChange={(e) => setFullName(e.target.value)} />}
         <input className='mx-5 p-3 my-4 w-10/12 md:w-11/12  md:mx-0 rounded-lg bg-gray-800 border-1' placeholder='Enter Email' type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
         <input className=" mx-5 p-3 my-4 w-10/12 md:w-11/12  md:mx-0 rounded-lg bg-gray-800" placeholder='Enter Password' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         <button type='submit' className='bg-red-800 mx-5 p-3 my-4 w-10/12 md:w-11/12 md:mx-0 rounded-lg ' disabled={loading}>{loading ? "Loading..." : isLogin ? "Sign In" : "Sign Up"}</button>
